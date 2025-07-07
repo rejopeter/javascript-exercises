ec17d298d5633cdd8eae764859a9bbb341f7bd13
Dr. J. R. Rejo Peter, BAMS <218668545+rejopeter@users.noreply.github.com>
2025-07-07 18:15:32 +0530

    phew

--- 05_joinStrings/joinStrings.js
+++ 05_joinStrings/joinStrings.js
@@ -9,6 +9,19 @@
 
 // Add your code right below, good luck!
 
+const firstName = "Carlos";
+const lastName = "Stevenson";
+const thisYear = 1965;
+const birthYear = 1947; 
+// At first, greeting may look like this:
+// const greeting = "Hello! My name is " + firstName + " " + last
+// Full name and age calculations
+const fullName = firstName + " " + lastName;
+const age = thisYear - birthYear;
+const greeting = "Hello! My name is " + fullName + " and I am " + age + " years old.";
+console.log(greeting);
+// At the end of the exercise, greeting may look like this:
+// const greeting = "Hello! My name is " + fullName + " and I am " + age + " years old.";
 
 
 
