c745bb7d06d13d58a73308abfa5955d63b803597
Dr. J. R. Rejo Peter, BAMS <218668545+rejopeter@users.noreply.github.com>
2025-07-18 06:33:55 +0530

    deneee

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,9 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  const c = ((f - 32) * (5/9));
+  return Math.round(c);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  const f = ((c * (9/5)) + 32);
+  return Math.round(f);
 };
 
+console.log(convertToCelsius(32))
+console.log(convertToFahrenheit(0))
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
