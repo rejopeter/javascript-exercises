f6c3a664f7e98b5965a5bee69457ae72d51c14e9
Dr. J. R. Rejo Peter, BAMS <218668545+rejopeter@users.noreply.github.com>
2025-07-18 05:46:35 +0530

    done

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
+const reverseString = function(text) {
+    return text.split("").reverse().join("`̀")
+}
+console.log(reverseString("apple"))
 
-};
+
+
+
+// const reverseString = function()
 
 // Do not edit below this line
 module.exports = reverseString;
