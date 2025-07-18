0b701926ee4ea740382fb81f987bf30fa9352c02
Dr. J. R. Rejo Peter, BAMS <218668545+rejopeter@users.noreply.github.com>
2025-07-18 06:06:58 +0530

    done

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,10 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    let sum = 0;
+    for (let i = a; i <= b; i++) {
+        sum = sum + i;
+    }
+    return sum
 };
-
+console.log(sumAll(1,4))
 // Do not edit below this line
 module.exports = sumAll;
