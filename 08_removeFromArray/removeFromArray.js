e6d54a7e6e95139694770414ec756873ecad97b3
Dr. J. R. Rejo Peter, BAMS <218668545+rejopeter@users.noreply.github.com>
2025-07-18 05:39:14 +0530

    done

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, value) {
+    for (let i = 0;i < arr.length; i++) {
+        if (i == value) {
+            arr.splice(i-1, 1)
+        }
+    }
+    return arr
 };
-
+console.log(removeFromArray([1, 2, 3, 4], 3))
 // Do not edit below this line
 module.exports = removeFromArray;
