93979e7ad00eee4f165d7211db9516d5b69e0bc1
Dr. J. R. Rejo Peter, BAMS <218668545+rejopeter@users.noreply.github.com>
2025-07-18 06:14:47 +0530

    donee

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,6 +1,10 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (year % 4 || year % 100 || year % 400) {
+        console.log(year + " is Leap")
+    } else {
+        console.log("Not a leap year")
+    }
 };
-
+leapYears(2000)
 // Do not edit below this line
 module.exports = leapYears;
